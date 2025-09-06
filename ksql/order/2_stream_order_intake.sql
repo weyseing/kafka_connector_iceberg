9000f0efc98b11fd09e7e349e7b4d73035faf7ee
Jeremy Heng <hengweyseing53@gmail.com>
2025-09-06 18:22:32 +0800

    wip upsert

--- ksql/order/2_stream_order_intake.sql
+++ ksql/order/2_stream_order_intake.sql
@@ -1,9 +1,13 @@
-CREATE STREAM stream_order_intake 
-WITH (KAFKA_TOPIC='stream_order_intake', value_format='AVRO') AS
-SELECT 
-    AFTER->id, AFTER->product, AFTER->amount, AFTER->buyer_id, AFTER->create_date, AFTER->update_date,
-    (ROWTIME + 8*60*60*1000) AS row_time
+CREATE STREAM stream_order_intake
+WITH (KAFKA_TOPIC='stream_order_intake', VALUE_FORMAT='AVRO') AS
+SELECT
+    AFTER->ID AS `id`,
+    AFTER->PRODUCT AS `product`,
+    AFTER->AMOUNT AS `amount`,
+    AFTER->BUYER_ID AS `buyer_id`,
+    AFTER->CREATE_DATE AS `create_date`,
+    AFTER->UPDATE_DATE AS `update_date`,
+    (ROWTIME + 8*60*60*1000) AS `row_time`
 FROM stream_order
-WHERE 1=1
-    AND AFTER IS NOT NULL
+WHERE AFTER IS NOT NULL
 EMIT CHANGES;
