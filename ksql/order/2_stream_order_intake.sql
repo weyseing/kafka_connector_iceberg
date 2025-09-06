1d60b72883864646bca195651b9a0dce196fabd6
Jeremy Heng <hengweyseing53@gmail.com>
2025-09-07 03:09:47 +0800

    upsert

--- ksql/order/2_stream_order_intake.sql
+++ ksql/order/2_stream_order_intake.sql
@@ -1,6 +1,7 @@
 CREATE STREAM stream_order_intake
-WITH (KAFKA_TOPIC='stream_order_intake', VALUE_FORMAT='AVRO') AS
+WITH (KAFKA_TOPIC='stream_order_intake', VALUE_FORMAT='AVRO', KEY_FORMAT='JSON') AS
 SELECT
+    STRUCT(`id` := AFTER->ID) AS `KEY`,
     AFTER->ID AS `id`,
     AFTER->PRODUCT AS `product`,
     AFTER->AMOUNT AS `amount`,
@@ -10,4 +11,5 @@ SELECT
     (ROWTIME + 8*60*60*1000) AS `row_time`
 FROM stream_order
 WHERE AFTER IS NOT NULL
+PARTITION BY STRUCT(`id` := AFTER->ID) 
 EMIT CHANGES;
